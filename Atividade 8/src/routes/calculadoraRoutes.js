c5cf2b1caa051b100af26586669e74daeedf5db1
andregadelhajr <andrrejunior@hotmail.com.br>
2024-06-06 22:14:39 -0300

    Atualização projeto final 2

--- Atividade 8/src/routes/calculadoraRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const calculadoraController = require('../controller/calculadoraController');
-
-router.get('/', calculadoraController.indexCalculadora);
-router.post('/calcular', calculadoraController.calcular);
-
-module.exports = router;
